402547bbf6c7161e5980f3740d476949a76e2222
arunbandari <arunbandari2@gmail.com>
2019-09-28 21:23:22 +0530

    Add styling to bookmarks list

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 const showTime = () => {
-    $('#clock').html(`${new Date().getHours()}:${new Date().getMinutes()}`); // 11:18:48 AM
+    const hours = new Date().getHours().toString().padStart(2,0);
+    const minutes = new Date().getMinutes().toString().padStart(2,0);
+    $('#clock').html(`${hours}:${minutes}`); // 21:04
 }
 const listBookmarks = () => {
     chrome.bookmarks.getChildren('2', (bms) => {
         let uiBms = '';
         bms.forEach(bm => {
             uiBms += `<div class="item">
-            <i class="large github middle aligned icon"></i>
+            <i class="large middle aligned icon"><img src="chrome://favicon/${bm.url}"></i>
             <div class="content">
-              <a class="header">${bm.title}</a>
+              <a class="header inline" href="${bm.url}" target="_blank">${bm.title}  <span class="ui mini label">${bm.url}</span></a>
             </div>
+            <i class="aligned large pen square icon secondary middle" style="color:#ddd"></i>
           </div>`;
         });
         $('#bmList').html(uiBms);
     })
 }
 $(document).ready(function(){
+    showTime();
     listBookmarks();
     setInterval(showTime, 1000);
     $("#addBookmark").click(function(){
@@ -25,9 +29,9 @@ $(document).ready(function(){
             onApprove : function() {
                 var $form = $('.content form'),
                 bookmark = $form.form('get values');
-                bookmark.parentId = 2;
-                chrome.bookmarks.create(allFields, (response) => {
-                    console.log(bookmark)
+                bookmark.parentId = '2';
+                chrome.bookmarks.create(bookmark, (response) => {
+                  listBookmarks();
                 });
             }
           })
