9676bcb0963b73f7eb87d2765fbf926f82006c13
arunbandari <arunbandari2@gmail.com>
2019-09-28 23:52:17 +0530

    Add ability to edit a bookmark

--- script.js
+++ script.js
@@ -3,6 +3,29 @@ const showTime = () => {
     const minutes = new Date().getMinutes().toString().padStart(2,0);
     $('#clock').html(`${hours}:${minutes}`); // 21:04
 }
+const showBookmarkModal = () => {
+    $('.ui.mini.modal')
+    .modal({
+        closable  : true,
+        onApprove : function() {
+            const $form = $('.content form'),
+            bookmark = $form.form('get values');
+            bookmark.parentId = '2';
+            const id = bookmark.id;
+            delete bookmark.id;
+            if (id) {
+                chrome.bookmarks.update(id, { title: bookmark.title, url: bookmark.url }, (response) => {
+                    listBookmarks();
+                });  
+            } else {
+                chrome.bookmarks.create(bookmark, (response) => {
+                    listBookmarks();
+                });
+            }
+        }
+      })
+    .modal('show');
+};
 const listBookmarks = () => {
     chrome.bookmarks.getChildren('2', (bms) => {
         let uiBms = '';
@@ -10,9 +33,10 @@ const listBookmarks = () => {
             uiBms += `<div class="item">
             <i class="large middle aligned icon"><img src="chrome://favicon/${bm.url}"></i>
             <div class="content">
-              <a class="header inline" href="${bm.url}" target="_blank">${bm.title}  <span class="ui mini label">${bm.url}</span></a>
+              <a class="header inline" href="${bm.url}" target="_blank">${bm.title} <span class="ui primary mini label">${bm.url}</span></a>
             </div>
-            <i class="aligned large pen square icon secondary middle" style="color:#ddd"></i>
+            <i class="aligned small edit outline icon secondary middle" bId="${bm.id}" id="editBm" style="color:#ddd"></i>
+            <i class="aligned small trash icon secondary middle" bId="${bm.id}" id="editBm" style="color:#ddd"></i>
           </div>`;
         });
         $('#bmList').html(uiBms);
@@ -22,19 +46,19 @@ $(document).ready(function(){
     showTime();
     listBookmarks();
     setInterval(showTime, 1000);
-    $("#addBookmark").click(function(){
-        $('.ui.mini.modal')
-        .modal({
-            closable  : true,
-            onApprove : function() {
-                var $form = $('.content form'),
-                bookmark = $form.form('get values');
-                bookmark.parentId = '2';
-                chrome.bookmarks.create(bookmark, (response) => {
-                  listBookmarks();
-                });
-            }
-          })
-        .modal('show');
+    $("#addBookmark").click(() => {
+        $('.content form')
+         .form('set values', {title: '', url: '', id: ''});
+        showBookmarkModal();
+    });
+    $(document).on('click','#editBm',function(event){
+        const element = event.target;
+        const id = element.getAttribute('bid');
+        chrome.bookmarks.get(id, (bm) => {
+            console.log(bm[0])
+            $('.content form')
+             .form('set values', bm[0]);
+            showBookmarkModal();
+        })
     });
   });
